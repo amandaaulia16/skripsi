4b34d332637bffa8fd451d25774703c3377e2a82
amandaaulia16 <102532702+amandaaulia16@users.noreply.github.com>
2025-10-02 15:05:29 +0700

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
 # Sidebar menu
 menu = st.sidebar.radio("Pilih Menu", [
     "Collecting Data", 
